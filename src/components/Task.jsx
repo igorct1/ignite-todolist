99eefbbb6061a8a006323b3762df00c04c82eb49
igorct1 <igorct77@gmail.com>
2023-01-09 20:39:42 -0300

    funcionalities

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,18 +1,44 @@
+import { Check, Trash } from "phosphor-react";
 import React from "react";
 
-import trash from "../assets/trash.png";
+import trash from "../assets/trash.svg";
 
 import styles from "./Task.module.css";
 
-export const Task = () => {
+export const Task = ({ task, onSelect, onDelete, isCompleted }) => {
+  function handleSelectedTask() {
+    onSelect(task.id);
+  }
+
+  function handleDeleteTask() {
+    onDelete(task.id);
+  }
+
   return (
     <div className={styles.cardTask}>
-      <button className={styles.cardCheck}></button>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto
-        repudiandae eos reiciendis quasi distinctio pariatur?
+      <div className={styles.checked}>
+        <button
+          onClick={handleSelectedTask}
+          className={
+            !isCompleted ? styles.normalButton : styles.completedButton
+          }
+        >
+          {isCompleted ? <Check size={15} /> : null}
+        </button>
+      </div>
+      <p
+        onClick={handleSelectedTask}
+        className={!isCompleted ? styles.normalItem : styles.selectedItem}
+      >
+        {task.title}
       </p>
-      <img src={trash} alt="" />
+      <button
+        className={styles.deleteButton}
+        src={trash}
+        onClick={handleDeleteTask}
+      >
+        <Trash size={18} />
+      </button>
     </div>
   );
 };
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { v4 as uuidv4 } from "uuid";
 import plus from "../assets/plus.png";
 import { DefaultTaskItem } from "./DefaultTaskItem";
 import { Task } from "./Task";
@@ -7,26 +7,104 @@ import { Task } from "./Task";
 import styles from "./Tasks.module.css";
 
 export const Tasks = () => {
+  const [tasks, setTasks] = React.useState([
+    {
+      id: uuidv4(),
+      title: "Estudar React.js na Rocketseat!",
+      isCompleted: true,
+    },
+    {
+      id: uuidv4(),
+      title: "Estudar TypeScript",
+      isCompleted: false,
+    },
+  ]);
+  const [newTask, setNewTask] = React.useState("");
+  const completedTasks = tasks.reduce((acc, item) => {
+    if (item.isCompleted) {
+      acc++;
+    }
+    return acc;
+  }, 0);
+
+  function handleNewTask({ target }) {
+    setNewTask(target.value);
+  }
+
+  function onAddTask() {
+    setTasks([
+      ...tasks,
+      {
+        id: uuidv4(),
+        title: newTask,
+        isCompleted: false,
+      },
+    ]);
+    setNewTask("");
+  }
+
+  function onSelect(taskId) {
+    const newTaskList = tasks.map((task) => {
+      if (task.id === taskId) {
+        task.isCompleted = !task.isCompleted;
+      }
+      return task;
+    });
+    setTasks(newTaskList);
+  }
+
+  function onDelete(taskId) {
+    const newTaskList = tasks.filter((task) => task.id !== taskId);
+    setTasks(newTaskList);
+  }
+
   return (
     <div className={styles.tasksContainer}>
       <div className={styles.inputContainer}>
-        <input type="text" placeholder="Adicione uma nova tarefa.." />
-        <button>
-          Criar <img src={plus} alt="" />
-        </button>
+        <input
+          type="text"
+          placeholder="Adicione uma nova tarefa.."
+          value={newTask}
+          onChange={handleNewTask}
+        />
+        {newTask.length > 0 ? (
+          <button onClick={onAddTask}>
+            Criar <img src={plus} alt="" />
+          </button>
+        ) : (
+          <button disabled onClick={onAddTask}>
+            Criar <img src={plus} alt="" />
+          </button>
+        )}
       </div>
       <div className={styles.createdAndCompletedTasks}>
         <header className={styles.createdAndCompletedTasksContainer}>
           <strong className={styles.createdTasks}>
-            Tarefas criadas <span>0</span>
+            Tarefas criadas <span>{tasks.length}</span>
           </strong>
           <strong className={styles.completedTasks}>
-            Concluídas <span>0 de 5</span>
+            Concluídas{" "}
+            <span>
+              {completedTasks} de {tasks.length}
+            </span>
           </strong>
         </header>
       </div>
       <div className={styles.tasksWrapper}>
-        <DefaultTaskItem />
+        {tasks.length ? (
+          tasks.map((task) => (
+            <Task
+              key={task.id}
+              task={task}
+              setTasks={setTasks}
+              onSelect={onSelect}
+              onDelete={onDelete}
+              isCompleted={task.isCompleted}
+            />
+          ))
+        ) : (
+          <DefaultTaskItem />
+        )}
       </div>
     </div>
   );
